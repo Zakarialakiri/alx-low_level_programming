e201e538dee31fc1c088b324e6fd465338077f4b
Zakarialakiri <zakarialakiri@gmail.com>
2023-07-13 16:09:45 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-/*usfslifsifs dzdzdzdzdzdz */
+/*Print the string "Programming is like building a multilingual puzzle*/
 int main(void)
-// Print the string "Programming is like building a multilingual puzzle"
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
