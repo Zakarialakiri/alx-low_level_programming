5eac3d5ae9a43a34ad1a699966ca7f888ba9f554
Zakarialakiri <zakarialakiri@gmail.com>
2023-07-16 15:38:04 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  * Function writes "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * followed by a newline to the standard error.
  *
- * Return: Always 0.
+ * Return: Always 1.
  */
 
 int main(void)
 {
 const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, sizeof(message) - 1);
-return(1);
+return (1);
 }
