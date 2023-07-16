d6cc256e52a11d56a05b7933239d9ed3abe3323d
Zakarialakiri <zakarialakiri@gmail.com>
2023-07-16 15:20:52 -0700

    Hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(void) {
-    printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-    return 0;
+int main(void)
+{
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(2, message, sizeof(message) - 1);
+return (1);
 }
