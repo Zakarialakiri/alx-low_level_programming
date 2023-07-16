98b2ea078f40173621305fb7addf71a9e07c62c4
Zakarialakiri <zakarialakiri@gmail.com>
2023-07-16 14:30:31 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program.
  *
- * This function prints the sentence "with proper grammar, but the outcome is a piece of art,"
+ * Function prints "with proper grammar, but the outcome is a piece of art,"
  * followed by a new line.
  *
  * Return: Always 0.
