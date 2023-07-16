1aa529ea322162034faf3c739eea3b0acd48e11b
Zakarialakiri <zakarialakiri@gmail.com>
2023-07-16 14:27:58 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints the sentence "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
