6bebe08f62cad2d1e213912880384b781b8c19c0
Zakarialakiri <zakarialakiri@gmail.com>
2023-07-18 14:01:19 -0700

    Hello

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,16 @@
 /**
 * _islower - check for lower character
 *@c: The character to be checked
-* Return : 1 for lowercase character or 0 for anything else
+* Return : 1 for _islower character or 0 for anything else
 */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
