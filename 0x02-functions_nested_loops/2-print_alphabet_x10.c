e90fd0818bd1690e477eb571c3987f72900e6704
Zakarialakiri <zakarialakiri@gmail.com>
2023-07-18 13:39:16 -0700

    Hello

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 void print_alphabet_x10(void)
 {
-    char letter;
-    int counter;
-
-    for (counter = 0; counter < 10; counter++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
+char letter;
+int counter;
+for (counter = 0; counter < 10; counter++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
     }
 }
